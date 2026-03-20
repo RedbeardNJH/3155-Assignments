17d058bfdb9ec6d298f84597185cd06c60e24df5
RedbeardNJH <noahjharden@gmail.com>
2026-03-19 23:38:59 -0400

    Building BaseModel for Items

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,6 +1,13 @@
+from typing import Union
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    isOffer: Union[bool, None] = None
 
 @app.get("/")
 def read_root():
